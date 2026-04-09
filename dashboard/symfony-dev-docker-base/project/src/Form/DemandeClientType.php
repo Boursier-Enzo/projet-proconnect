564e40736d7a261657f324e14d19819a4da086a3
KillianM18 <killian.mahut@sfr.fr>
2026-04-10 01:10:16 +0200

    refactor demande_client UI, move CSS to app.css, add datetime and select fields

--- dashboard/symfony-dev-docker-base/project/src/Form/DemandeClientType.php
+++ dashboard/symfony-dev-docker-base/project/src/Form/DemandeClientType.php
@@ -8,29 +8,42 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class DemandeClientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('objet')
-            ->add('description')
-            ->add('typePrestation')
-            ->add('creneauSouhaite')
-            ->add('statut')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('architecte', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
-        ;
-    }
+{
+    $builder
+        ->add('objet')
+        ->add('description')
+        ->add('typePrestation', ChoiceType::class, [
+            'choices'  => [
+                'Installation' => 'Installation',
+                'Maintenance' => 'Maintenance',
+                'Dépannage' => 'Dépannage',
+                'Audit' => 'Audit',
+            ],
+        ])
+        ->add('creneauSouhaite', DateTimeType::class, [
+            'widget' => 'single_text',
+            'required' => true,
+        ])
+        ->add('statut', ChoiceType::class, [
+            'choices'  => [
+                'En attente' => 'non_accepte',
+                'Accepté' => 'accepte',
+            ],
+        ])
+        ->add('architecte', EntityType::class, [
+            'class' => User::class,
+            'choice_label' => 'email',
+            'placeholder' => 'Choisir un architecte...',
+            'required' => false,
+        ])
+    ;
+}
 
     public function configureOptions(OptionsResolver $resolver): void
     {
