901c5891d6e966ae0dbd31ccda31df0ac40f2a54
Adrien337 <adrien.cros2007@gmail.com>
2026-04-09 10:42:57 +0200

    Footer + Conditions utilisation créees

--- client/php-docker-base-dev/www/index.php
+++ client/php-docker-base-dev/www/index.php
@@ -1,3 +1,17 @@
-<?php
-echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ProConnect</title>
+</head>
+<body>
+    <header>
+        <!-- <?php include 'ui/header.php';?> -->
+    </header>
+    <main>
+        <!-- Main content -->
+    </main>
+    <?php include 'ui/footer.php';?>
+</body>
+</html>
---
+++ client/php-docker-base-dev/www/ui/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <footer>
+        <div class="footer-logo">
+            <img src="" alt="Logo ProConnect">
+        </div>
+        <div class="footer-content">
+            <div class="footer-title">
+                <h2>ProConnect</h2>
+            </div>
+            <div class="footer-text">
+                <div class="conditions-utilisation">
+                    <div class="conditions-utilisation-title">
+                        <a href="../conditions-utilisation.php"><h3>Conditions d'utilisation</h3></a>
+                    </div>
+                </div>
+                <div class="">[Content 2]</div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
